878b0a165d012f771c126812874fd20b63d23bee
feevan <timsteam1111@gmail.com>
2025-10-12 21:39:37 +0300

    Did open of the settings

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,13 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Game : MonoBehaviour
 {
     [SerializeField] private GameObject mushroom;
     [SerializeField] private GameObject buttonStart;
+    [SerializeField] private GameObject buttonSettings;
+    [SerializeField] private GameObject buttonLanguedge;
+    [SerializeField] private GameObject settingsScreen;
+    [SerializeField] private GameObject buttonBackFromSettings;
+
+    private void Start()
+    {
+        buttonLanguedge.SetActive(false);
+        settingsScreen.SetActive(false);
+        buttonBackFromSettings.SetActive(false);
+    }
 
     public void OnButtonStartClick()
     {
         mushroom.SetActive(false);
         buttonStart.SetActive(false);
+    }
+
+    public void OnSettingsClick()
+    {
+        mushroom.SetActive(false);
+        buttonStart.SetActive(false);
+        buttonBackFromSettings.SetActive(true);
+        buttonSettings.SetActive(false);
+        settingsScreen.SetActive(true);
+        buttonLanguedge.SetActive(true);
+
     }
 }
